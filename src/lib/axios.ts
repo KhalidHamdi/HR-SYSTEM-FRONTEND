5ac9a098f84b12e98f244e8b66e8099b93f8bbdc
khalidhamdi <khalidhamdi204@gmail.com>
2025-01-11 11:02:48 +0200

    fix: fix bug

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,8 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  // baseURL: 'http://localhost:8000/api',
-  baseURL: 'https://hr-system-backend.vercel.app/',
+  baseURL: 'http://localhost:8000/api',
 });
 
 api.interceptors.request.use((config) => {
